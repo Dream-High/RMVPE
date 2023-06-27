b38c75b9a30168f6f4a0b42ec0c3d11b0ea950f9
Dream-High <2360513980@qq.com>
2023-06-27 17:28:36 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from .dataset import MIR1K, MIR_ST500, MDB
 from .constants import *
-from .model import E2E
+from .model import E2E, E2E0
 from .utils import cycle, summary, to_local_average_cents
 from .loss import FL, bce, smoothl1
+from .inference import Inference
